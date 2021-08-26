6a395a3fcafdb63be4266f9b55e31c064ef17088
Jaimes77 <JaimeRGoff@gmail.com>
2021-08-26 17:50:55 -0400

    made changes per Taylor

--- workout-logbook/server/controllers/usercontroller.js
+++ workout-logbook/server/controllers/usercontroller.js
@@ -7,7 +7,7 @@ const bcrypt = require("bcryptjs");
 router.post("/register", function (req, res) {
   User.create({
     username: req.body.user.username,
-    passwordhash: bcrypt.hashSync(req.body.user.password, 13),
+    passwordhash: bcrypt.hashSync(req.body.user.passwordhash, 13),
   })
     .then(function createSuccess(user) {
       let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
@@ -32,8 +32,8 @@ router.post("/login", function (req, res) {
     .then(function loginSuccess(user) {
       if (user) {
         bcrypt.compare(
-          req.body.user.password,
-          user.passwordhash,
+          req.body.user.passwordhash,
+          user.password,
           function (err, matches) {
             if (matches) {
               let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
